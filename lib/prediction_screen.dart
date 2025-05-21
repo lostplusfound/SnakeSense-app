2cae9b720013e4a773d2a93c2215dfdbfb85b3d1
lostplusfound <57611739+lostplusfound@users.noreply.github.com>
2025-05-21 14:18:24 -0700

    Moved species predictor code from SpeciesPredictor to PredictionScreen

--- lib/prediction_screen.dart
+++ lib/prediction_screen.dart
@@ -1,8 +1,9 @@
-import 'dart:typed_data';
 import 'package:cross_file/cross_file.dart';
+import 'package:csv/csv.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:is_it_poisonous/onnx_predictor.dart';
 import 'package:is_it_poisonous/species.dart';
-import 'package:is_it_poisonous/species_predictor.dart';
 
 class PredictionScreen extends StatefulWidget {
   final XFile _image;
@@ -14,24 +15,40 @@ class PredictionScreen extends StatefulWidget {
 }
 
 class _PredictionScreenState extends State<PredictionScreen> {
+  static final List<Species> _speciesList = [];
   Uint8List? _imageBytes;
   Species? _predictedSpecies;
 
   @override
   void initState() {
     super.initState();
-    _loadImage();
-    _predictSpecies();
+    _loadImageAndPredict();
   }
 
-  void _loadImage() async {
+  void _loadImageAndPredict() async {
     _imageBytes = await widget._image.readAsBytes();
+    _predictedSpecies = await getPrediction(widget._image);
     setState(() {});
   }
 
-  void _predictSpecies() async {
-    _predictedSpecies = await SpeciesPredictor.getPrediction(widget._image);
-    setState(() {});
+  static Future<void> _loadSpecies() async {
+    final csvString = await rootBundle.loadString('assets/csv/species.csv');
+    List<List<dynamic>> rows = const CsvToListConverter(textDelimiter: null, eol: '\n').convert(csvString).sublist(1);
+    for (List<dynamic> row in rows) {
+      _speciesList.add(Species.fromCsvRow(row));
+    }
+  }
+
+  static Future<Species> getPrediction(XFile image) async {
+    if (_speciesList.isEmpty) {
+      await _loadSpecies();
+    }
+    List preds = await OnnxPredictor.predict(image);
+    int predictedIndex = 0;
+    for (int i = 0; i < preds.length; i++) {
+      if (preds[i] > preds[predictedIndex]) predictedIndex = i;
+    }
+    return _speciesList[predictedIndex];
   }
 
   @override
