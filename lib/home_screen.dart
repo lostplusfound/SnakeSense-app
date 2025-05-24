8a1c3b013eaaf6627c071ba392f4bef969bf7185
lostplusfound <57611739+lostplusfound@users.noreply.github.com>
2025-05-23 20:01:49 -0700

    Changed app name to SnakeSense and changed HomeScreen to StatelessWidget

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,33 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:is_it_poisonous/classify_screen.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  int _selectedIndex = 0;
-  final List<Widget> destinations = [ClassifyScreen(),];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Is it Poisonous?')),
-      body: destinations[_selectedIndex],
-      bottomNavigationBar: NavigationBar(
-        selectedIndex: _selectedIndex,
-        onDestinationSelected: (int index) {
-          setState(() {
-            _selectedIndex = index;
-          });
-        },
-        destinations: [
-          NavigationDestination(icon: Icon(Icons.search), label: 'Classify'),
-        ],
-      ),
+      body: ClassifyScreen()
     );
   }
 }
