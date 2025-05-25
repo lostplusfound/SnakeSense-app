0a16f162b6635c98078c4b6d85b895175066e844
lostplusfound <57611739+lostplusfound@users.noreply.github.com>
2025-05-25 12:37:07 -0700

    Added AboutListTile

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:is_it_poisonous/classify_screen.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -7,8 +8,39 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            AboutListTile(
+              applicationName: 'SnakeSense',
+              applicationVersion: '1.0.0',
+              applicationLegalese: '© 2025 lostplusfound',
+              aboutBoxChildren: [
+                TextButton(
+                  onPressed: () async {
+                    if (await canLaunchUrlString('https://github.com/lostplusfound/SnakeSense-app')) {
+                      await launchUrlString('https://github.com/lostplusfound/SnakeSense-app');
+                    } else {
+                      if(context.mounted) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text('Could not open the link.'),
+                          behavior: SnackBarBehavior.floating,
+                        ),
+                      );
+                      }
+                    }
+                  },
+
+                  child: Text('Github Repository'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
       appBar: AppBar(title: Text('SnakeSense')),
-      body: ClassifyScreen()
+      body: ClassifyScreen(),
     );
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,9 +8,11 @@ import Foundation
 import file_selector_macos
 import flutter_onnxruntime
 import path_provider_foundation
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FlutterOnnxruntimePlugin.register(with: registry.registrar(forPlugin: "FlutterOnnxruntimePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
